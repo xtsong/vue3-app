01e08f20cc4e45b1d938cdfb6d4007db56eff0db
xtsong <02087790@ytoexpress.com>
2023-03-10 09:33:07 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <van-grid :column-num="3">
+      <van-grid-item v-for="value in 6" :key="value" icon="photo-o" text="文字" />
+    </van-grid>
+  </div>
+</template>
+
+<script>
+import { Grid, GridItem } from 'vant';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    [Grid.name]: Grid,
+    [GridItem.name]: GridItem
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
